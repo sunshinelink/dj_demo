f92c58a7bfbdf23bab86bd2516004e9f37bd27eb
sunnylink <794724039@qq.com>
2017-02-17 15:50:13 +0800

    201702171549_add_bootstrap

--- dj_demo/dj_demo/urls.py
+++ dj_demo/dj_demo/urls.py
@@ -6,4 +6,5 @@ from mychat import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$',views.index),
+    url(r'^static/(?P<path>.*)$','django.contrib.staticfiles.views.serve')
 ]
